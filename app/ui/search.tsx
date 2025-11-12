cb814afe6e9d35370a3165373dc6a5e57109c1ec
umiteru2004 <umiteru2004@gmail.com>
2025-11-12 11:18:53 +0900

    第10章：検索パラメータでURLを更新

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
