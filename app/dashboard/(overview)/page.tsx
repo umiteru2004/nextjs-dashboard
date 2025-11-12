a042853179613748b6198b8ff18927a37da15ab8
umiteru2004 <umiteru2004@gmail.com>
2025-11-12 09:24:45 +0900

    第9章：コンポーネントのストリーミング（練習）

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,11 +3,13 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
-import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
+import { fetchCardData } from "@/app/lib/data";
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -34,7 +36,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
