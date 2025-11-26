4ca1fb03df622d826d196d36a1fabcd39ba9b440
umiteru2004 <umiteru2004@gmail.com>
2025-11-26 13:40:14 +0900

    第10章：デバウンス（関数呼び出し頻度の制限）

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,16 @@
 
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
 
     if (term) {
@@ -18,7 +21,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
