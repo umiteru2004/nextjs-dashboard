5ff882a7e744bbef5261b67477f72bbf8aa0dc9f
umiteru2004 <umiteru2004@gmail.com>
2025-11-27 15:10:41 +0900

    第11章：再検証とリダイレクト（ルーターキャッシュの削除）

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod";
 
@@ -29,4 +31,7 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
