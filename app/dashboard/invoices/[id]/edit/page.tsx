ef0f62a8d0e59235cc5e91c4cd8158fd0671f7b4
umiteru2004 <umiteru2004@gmail.com>
2025-11-27 15:50:01 +0900

    第11章：特定の請求書データを取得

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,10 +1,14 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchCustomers } from "@/app/lib/data";
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
 
   return (
     <main>
