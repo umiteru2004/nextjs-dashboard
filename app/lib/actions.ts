3b54f340c280ec5e29b5fe1f555b021241a0d597
umiteru2004 <umiteru2004@gmail.com>
2025-12-03 11:29:19 +0900

    fix: 請求書フォームのアクション関数から返り値を削除しました。

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -36,9 +36,6 @@ export async function createInvoice(formData: FormData) {
   } catch (error) {
     // We'll also log the error to the console for now
     console.error(error);
-    return {
-      message: "Database Error: Failed to Create Invoice.",
-    };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -63,7 +60,6 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     // We'll also log the error to the console for now
     console.error(error);
-    return { message: "Database Error: Failed to Update Invoice." };
   }
 
   revalidatePath("/dashboard/invoices");
